ff34c78877b2ae1538ff5e44001c2cd5a18fef53
Don Delice Dushime <107407612+dondelice257@users.noreply.github.com>
2023-01-03 11:32:09 +0200

    Create script.js
    This is the script of my project

---
+++ script.js
@@ -0,0 +1,24 @@
+const list = document.querySelector("ol"); 
+const input = document.querySelector("input"); 
+const button = document.querySelector("button");
+function action() { 
+  if(input.value != ""){ 
+    const newEl = document.createElement("li"); 
+    newEl.innerHTML=input.value; 
+    list.appendChild(newEl); 
+    const newi = document.createElement("span"); 
+    newi.innerHTML="x" newEl.appendChild(newi); 
+    const close = document.querySelectorAll("span"); 
+    for(let i=0; i<close.length; i++){ 
+      close[i].addEventListener("click", function(){ 
+        close[i].parentElement.style.opacity="0" 
+        setTimeout(function () { 
+          close[i].parentElement.style.display="none" 
+        }
+                  ) 
+      }
+                               ) 
+    } 
+  } 
+} 
+button.addEventListener("click", action)
