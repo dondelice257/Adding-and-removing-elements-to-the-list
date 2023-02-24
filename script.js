07e2064d0233bb72310ab5d96f81c11628d7297a
Don Delice Dushime <107407612+dondelice257@users.noreply.github.com>
2023-02-24 13:21:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const list = document.querySelector("ol"); 
 const input = document.querySelector("input"); 
-const button = document.querySelector("button");
+const button = document.querySelector("button");2
 function action() { 
   if(input.value != ""){ 
     const newEl = document.createElement("li"); 
